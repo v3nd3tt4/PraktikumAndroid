79a75cc200ba7a5c82b0a6b689bd03c1c2c84ffb
Okta Pilopa <okta.pilopa@staff.itera.ac.id>
2024-11-10 20:53:17 +0800

    done modul pertama

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,5 @@
 import { Stack } from "expo-router";
 
-export default function RootLayout(){
-    return (
-        <Stack>
             <Stack.Screen name="(tabs)" options={{headerShown:false}}/>
-            <Stack.Screen name="details/[id]" options={{
-                headerTitle:'Detail',
-                headerTransparent: true,
-                headerTintColor: '#fff',
-            }}
             />
-        </Stack>
-    );
 }
